962088f9f8c0ef76f25f840442595c59be552650
Witoslaw Koczewski <wi@koczewski.de>
2020-09-23 10:30:41 +0200

    emacs config

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((clojure-mode . ((cider-preferred-build-tool          . clojure-cli)
+                  (cider-clojure-cli-global-options    . "-A:fig:dev")
+                  (cider-default-cljs-repl             . figwheel-main)
+                  (cider-figwheel-main-default-options . "dev"))))
